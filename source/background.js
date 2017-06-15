014b9edb8a473f9aea4851dc410773ecd53d5f16
moghya <sawantshubham571@gmail.com>
2017-06-16 01:38:16 +0530

    added icon and deployed

--- source/background.js
+++ source/background.js
@@ -15,18 +15,16 @@ function downloadCourse(courseCode){
 	var courseLink = 'http://nptel.ac.in/courses/'+courseCode+'/';
 		$.get( courseLink, 	
 			function( data ) {
-				try{
-					var title = $(data).filter('title').text();
-					console.log(title);
-					var scriptTag = $(data).filter('[type="application/ld+json"]').text();
-					var courseDetails = JSON.parse(scriptTag);
-					console.log(courseDetails);
-					var lectures = courseDetails.partOfEducationalTrack;
-					for(var i=0;i<lectures.length;i++)
-					downloadVideo(courseCode,i+1,lectures[i]);
-				}catch{
-					console.log('Error Occured');
-				}
+
+				var title = $(data).filter('title').text();
+				console.log(title);
+				alert('Downloadin Course: '+title);
+				var scriptTag = $(data).filter('[type="application/ld+json"]').text();
+				var courseDetails = JSON.parse(scriptTag);
+				console.log(courseDetails);
+				var lectures = courseDetails.partOfEducationalTrack;
+				for(var i=0;i<lectures.length;i++)
+				downloadVideo(courseCode,i+1,lectures[i]);
 			}
 		);
 }
@@ -35,21 +33,17 @@ function downloadCourse(courseCode){
 function downloadVideo(courseCode,lec,pageLink){
 	$.get( pageLink,	
 		function( data ) {
-			try{	
-				var downloadLink = $(data).find('#download')[0].childNodes[5].childNodes[3].href;
-				console.log(downloadLink);
-				var videoName = getParameterByName('subjectName',downloadLink);
-				//videoName = videoName.replace(' ','_');
-				nameOfFile = courseCode + '_' + lec + '_'+ videoName;
-				nameOfFile = nameOfFile.replace(/[^A-Z0-9]/ig, "_")+'.mp4';
-				console.log(nameOfFile);
-				chrome.downloads.download({
-				  url: downloadLink,
-				  filename: nameOfFile
-				});	
-			}catch{
-				console.log('Error Occurred');
-			}			
+			
+			var downloadLink = $(data).find('#download')[0].childNodes[5].childNodes[3].href;
+			console.log(downloadLink);
+			var videoName = getParameterByName('subjectName',downloadLink);
+			nameOfFile = courseCode + '_lec_' + lec + '_'+ videoName;
+			nameOfFile = nameOfFile.replace(/[^A-Z0-9]/ig, "_")+'.mp4';
+			console.log(nameOfFile);
+			chrome.downloads.download({
+			  url: downloadLink,
+			  filename: nameOfFile
+			});				
 		}
 	);
 }
